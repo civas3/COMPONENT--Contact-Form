51735fb64ce7641c4e0a23c0110ad9a434000eba
Paul C <57236805+civas3@users.noreply.github.com>
2021-06-15 16:28:44 +0100

    contact form validation
    vanila js

---
+++ js/contact-form-validation.js
@@ -0,0 +1,93 @@
+// Defining a function to display error message
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+  }
+  
+  // Defining a function to validate form 
+  document.getElementById("contactForm").addEventListener("submit",function(e) {
+    // Retrieving the values of form elements 
+    let fullname = this.fullname.value;
+    let email = this.email.value;
+    let country = this.country.value;
+    let subject = this.subject.value;
+    let message = this.message.value;
+
+    console.log(fullname);
+    console.log(email);
+    console.log(country);
+    console.log(subject);
+    console.log(message);
+  
+  
+    // Defining error variables with a default value
+    let nameValidation = emailValidation = countryValidation = subjectValidation = messageValidation = true;
+  
+  
+    // Validate name
+    printError("nameValidation", "");
+    if (fullname == "") {
+      printError("nameValidation", "Please enter your name");
+      nameValidation = false;
+    } else {
+      let regex = /^[a-zA-Z\s]+$/;
+      if (regex.test(fullname) === false) {
+        printError("nameValidation", "Please enter a valid name");
+        nameValidation = false;
+      }
+    }
+  
+    // Validate email address
+    printError("emailValidation", "");
+    if (email == "") {
+      printError("emailValidation", "Please enter your email address");
+      emailValidation = false;
+    } else {
+      // Regular expression for basic email validation
+      let regex = /^\S+@\S+\.\S+$/;
+      if (regex.test(email) === false) {
+        printError("emailValidation", "Please enter a valid email address");
+        emailValidation = false;
+      }
+    }
+  
+    // Validate country
+    printError("countryValidation", "");
+    if (country == "Select") {
+      printError("countryValidation", "Please select your country");
+      countryValidation = false;
+    }
+  
+  
+  
+    // Validate subject
+    printError("subjectValidation", "");
+    if (subject == "Select") {
+      printError("subjectValidation", "Please select the subject");
+      subjectValidation = false;
+    }
+
+    // Validate message
+    printError("messageValidation", "");
+    if (message == "") {
+      printError("messageValidation", "Please enter a message");
+      messageValidation = false;
+    }
+  
+  
+  
+    // Prevent the form from being submitted if there are any errors
+    if (!nameValidation || !emailValidation || !countryValidation || !subjectValidation || !messageValidation) {
+      e.preventDefault(); // stop submission
+      return false;
+    }
+    // Creating a string from input data for preview
+    let dataPreview = "You've entered the following details: \n" +
+      "Full Name: " + fullname + "\n" +
+      "Email Address: " + email + "\n" +
+      "Country: " + country + "\n" +
+      "Subject: " + subject + "\nSubmit?";
+    if (!confirm(dataPreview)) {
+      e.preventDefault(); // stop submission
+    }
+  });
+ 
